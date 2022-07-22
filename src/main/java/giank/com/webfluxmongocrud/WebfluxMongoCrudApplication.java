b0a5bd31c77020f36b0b8cb3cd95ad327e36ee10
gperezmal <e_gpmalca@latam.stefanini.com>
2022-07-21 20:10:28 -0500

    mongoDB-Webflux

---
+++ src/main/java/giank/com/webfluxmongocrud/WebfluxMongoCrudApplication.java
@@ -0,0 +1,13 @@
+package giank.com.webfluxmongocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxMongoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxMongoCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/giank/com/webfluxmongocrud/WebfluxMongoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package giank.com.webfluxmongocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxMongoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
